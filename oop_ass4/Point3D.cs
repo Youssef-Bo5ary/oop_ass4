9d6483ad1eae85c0727d20aa781139d2b6d1db0c
youusseff <youusseff@DESKTOP-6M24MB9>
2024-07-23 19:36:53 +0300

    Add project files.

---
+++ oop_ass4/Point3D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_ass4
+{
+    internal class Point3D : IComparable
+    {
+       
+
+        public double Dim01 {  get; set; }
+        public double Dim02 { get; set; } 
+        public double Dim03 { get; set; }
+
+       
+       
+
+        public Point3D(double dim01, double dim02, double dim03)
+        {
+            Dim01 = dim01;
+            Dim02 = dim02;
+            Dim03 = dim03;
+        }
+        public override string ToString()
+        {
+            return $"Point coordinates: ({Dim01} ,{Dim02},{Dim03})";
+
+        }
+
+       
+
+        public int CompareTo(object? obj)
+        {
+            Point3D p = (Point3D)obj;
+            if (this.Dim01 > p.Dim01 && this.Dim02 > p.Dim02) return 1;
+            else if (this.Dim01 < p.Dim01 && this.Dim02 < p.Dim02) return -1;
+            else return 0;
+        }
+    }
+}
---
+++ oop_ass4/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace oop_ass4
+{
+    internal class Program
+    {
+        public static decimal ReadDecimalFromUser(string message)
+        {
+            decimal number;
+            bool validInput = false;
+
+            do
+            {
+                Console.Write(message);
+                string userInput = Console.ReadLine();
+                validInput = decimal.TryParse(userInput, out number);
+
+                if (!validInput)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid decimal number.");
+                }
+            }
+            while (!validInput);
+
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            #region Question2
+            //Point3D p = new Point3D(10, 10, 10);
+            //Console.WriteLine(p.ToString); 
+            #endregion
+
+            #region Question3&4
+            //decimal dim1 = ReadDecimalFromUser("Enter the first decimal number: ");
+            //decimal dim2 = ReadDecimalFromUser("Enter the second decimal number: ");
+            //decimal dim3 = ReadDecimalFromUser("Enter the third decimal number: ");
+            //Point3D p1 = new Point3D(dim1, dim2, dim3);
+            //decimal num1 = ReadDecimalFromUser("Enter the first decimal number: ");
+            //decimal num2 = ReadDecimalFromUser("Enter the second decimal number: ");
+            //decimal num3 = ReadDecimalFromUser("Enter the third decimal number: ");
+            //Point3D p2 = new Point3D(num1, num2, num3);
+
+            //if (p1 == p2)
+            //{
+            //    Console.WriteLine(true);
+
+            //} 
+            #endregion
+
+            #region Define an array of points and sort this array based on X & Y coordinates.
+
+            Point3D[] points = new Point3D[3];
+            points[0]=new Point3D(1,2,3);
+            points[1]=new Point3D(10,20,30);
+            points[2] = new Point3D(100, 200, 300);
+            Array.Sort(points);
+            
+            
+            
+
+                
+            
+            
+            
+            
+
+            
+            
+            
+               
+            
+            
+            
+            #endregion
+
+
+        }
+    }
+}
+    
+
